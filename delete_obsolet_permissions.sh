e2d4053d909ce4746e51e833eb6e0f957c824f97
Henrique Batista <henrique.batista.liduario@gmail.com>
2024-10-11 11:02:30 +0100

    Update delete_obsolet_permissions.sh

--- delete_obsolet_permissions.sh
+++ delete_obsolet_permissions.sh
@@ -2,21 +2,8 @@
 
 # Permissões problemáticas a serem removidas
 permissions_to_remove=(
-  "access memcache statistics"
-  "access slab cachedump"
-  "administer google tag manager"
-  "administer hsts"
-  "administer migrations"
   "clear caches"
-  "create encontros_2020 site setting"
-  "delete encontros_2020 site setting"
-  "disable textfield module"
-  "edit encontros_2020 site setting"
-  "restful get user_picture_update"
-  "restful patch person_picture_update"
-  "restful post user_picture_update"
-  "view published encontros_2020 site setting entities"
-  "view unpublished encontros_2020 site setting entities"
+  ""
 )
 
 # Conectar ao banco de dados e remover associações de permissões
