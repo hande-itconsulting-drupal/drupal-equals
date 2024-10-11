f40acbce6730a17e77617dd874627d40a6e59ebb
Henrique Batista <henrique.batista.liduario@gmail.com>
2024-10-11 10:55:37 +0100

    Update delete_site_settings.sh

--- delete_site_settings.sh
+++ delete_site_settings.sh
@@ -5,21 +5,7 @@ entity_type="site_setting_entity"
 
 # Array de tipos de "Site Setting" a ser deletado
 site_setting_types=(
-    "aboutme_blacklist" #AboutME Blacklist
-    "banner_feed" #Banner Feed
-    "chat_call_settings" #Chat&Call Settings
-    "chatbot_settings" #Chatbot Settings
-    "code_of_ethics_title" #Code of Ethics
-    "edpon" #edpON TV
-    "facebook_workplace" #Facebook Workplace
-    "first_login_video" #Pop-up
-    "information" #Information Widget
-    "magazine" #Magazine
-    "mails_report" #Mails Report
-    "redirect_employee_app" #Redirect Employee App
-    "solidary_widget" #Solidary Widget
-    "user_preferences" #User Preferences
-    "widgets_default" #Widgets Default
+   
 )
 
 # Loop através de cada tipo de "Site Setting", deletar as entidades associadas
